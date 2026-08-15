789e39c14c7ed01cac1f83c5907100d16c2d87d6
Trevor Gross <tg@trevorgross.com>
2026-08-15 06:39:31 -0400

    Extract some reusable bits to separate functions

--- src/bytes.rs
+++ src/bytes.rs
@@ -167,7 +167,7 @@ pub fn split(in_bytes: &[u8]) -> Option<Vec<Vec<u8>>> {
 /// The string equivalent is [`shlex::Quoter`].
 #[derive(Default, Debug, Clone)]
 pub struct Quoter {
-    allow_nul: bool,
+    pub(crate) allow_nul: bool,
     // TODO: more options
 }
 
@@ -230,7 +230,7 @@ impl Quoter {
 }
 
 #[derive(PartialEq)]
-enum QuotingStrategy {
+pub(crate) enum QuotingStrategy {
     /// No quotes and no backslash escapes.  (If backslash escapes would be necessary, we use a
     /// different strategy instead.)
     Unquoted,
@@ -343,6 +343,36 @@ fn double_quoted_ok(c: u8) -> bool {
     }
 }
 
+pub(crate) const UNQUOTED_OK: u8 = 1;
+pub(crate) const SINGLE_QUOTED_OK: u8 = 2;
+pub(crate) const DOUBLE_QUOTED_OK: u8 = 4;
+
+pub(crate) fn check_char_quoting(c: u8, cur_ok: &mut u8) {
+    debug_assert!(c < 0x80);
+    if !unquoted_ok_fast(c) {
+        *cur_ok &= !UNQUOTED_OK;
+    }
+    if !single_quoted_ok(c) {
+        *cur_ok &= !SINGLE_QUOTED_OK;
+    }
+    if !double_quoted_ok(c) {
+        *cur_ok &= !DOUBLE_QUOTED_OK;
+    }
+}
+
+/// Pick the best allowed strategy.
+pub(crate) fn strategy_from_flags(prev_ok: u8) -> QuotingStrategy {
+    if prev_ok & UNQUOTED_OK != 0 {
+        QuotingStrategy::Unquoted
+    } else if prev_ok & SINGLE_QUOTED_OK != 0 {
+        QuotingStrategy::SingleQuoted
+    } else if prev_ok & DOUBLE_QUOTED_OK != 0 {
+        QuotingStrategy::DoubleQuoted
+    } else {
+        unreachable!()
+    }
+}
+
 /// Given an input, return a quoting strategy that can cover some prefix of the string, along with
 /// the size of that prefix.
 ///
@@ -350,10 +380,6 @@ fn double_quoted_ok(c: u8) -> bool {
 /// Postcondition: returned size is nonzero.
 #[cfg_attr(manual_codegen_check, inline(never))]
 fn quoting_strategy(in_bytes: &[u8]) -> (usize, QuotingStrategy) {
-    const UNQUOTED_OK: u8 = 1;
-    const SINGLE_QUOTED_OK: u8 = 2;
-    const DOUBLE_QUOTED_OK: u8 = 4;
-
     let mut prev_ok = SINGLE_QUOTED_OK | DOUBLE_QUOTED_OK | UNQUOTED_OK;
     let mut i = 0;
 
@@ -375,15 +401,7 @@ fn quoting_strategy(in_bytes: &[u8]) -> (usize, QuotingStrategy) {
             // has additional characters satisfying `isblank`.
             cur_ok &= !UNQUOTED_OK;
         } else {
-            if !unquoted_ok_fast(c) {
-                cur_ok &= !UNQUOTED_OK;
-            }
-            if !single_quoted_ok(c){
-                cur_ok &= !SINGLE_QUOTED_OK;
-            }
-            if !double_quoted_ok(c) {
-                cur_ok &= !DOUBLE_QUOTED_OK;
-            }
+            check_char_quoting(c, &mut cur_ok);
         }
 
         if cur_ok == 0 {
@@ -397,16 +415,7 @@ fn quoting_strategy(in_bytes: &[u8]) -> (usize, QuotingStrategy) {
         i += 1;
     }
 
-    // Pick the best allowed strategy.
-    let strategy = if prev_ok & UNQUOTED_OK != 0 {
-        QuotingStrategy::Unquoted
-    } else if prev_ok & SINGLE_QUOTED_OK != 0 {
-        QuotingStrategy::SingleQuoted
-    } else if prev_ok & DOUBLE_QUOTED_OK != 0 {
-        QuotingStrategy::DoubleQuoted
-    } else {
-        unreachable!()
-    };
+    let strategy = strategy_from_flags(prev_ok);
     debug_assert!(i > 0);
     (i, strategy)
 }
@@ -464,7 +473,7 @@ pub fn try_quote(in_bytes: &[u8]) -> Result<Cow<'_, [u8]>, QuoteError> {
 }
 
 #[cfg(test)]
-const INVALID_UTF8: &[u8] = b"\xa1";
+pub(crate) const INVALID_UTF8: &[u8] = b"\xa1";
 
 #[test]
 #[allow(invalid_from_utf8)]
--- src/lib.rs
+++ src/lib.rs
@@ -258,9 +258,8 @@ fn test_lineno() {
     }
 }
 
-#[test]
-#[cfg_attr(not(feature = "std"), allow(unreachable_code, unused_mut))]
-fn test_quote() {
+#[cfg(test)]
+fn test_cases() -> impl Iterator<Item = (String, String)> {
     // This is a list of (unquoted, quoted) pairs.
     // But it's using a single long (raw) string literal with an ad-hoc format, just because it's
     // hard to read if we have to put the test strings through Rust escaping on top of the escaping
@@ -293,24 +292,36 @@ fn test_quote() {
         <'$>              => <"'"'$'>
         <"^>              => <'"''^'>
     "#;
-    let mut ok = true;
-    for test in tests.trim().split('\n') {
-        let parts: Vec<String> = test
-            .replace("NL", "\n")
+
+    tests.trim().split('\n').map(|test| {
+        let parts = test.replace("NL", "\n");
+        let mut parts = parts
             .split("=>")
-            .map(|part| part.trim().trim_start_matches('<').trim_end_matches('>').to_owned())
-            .collect();
-        assert!(parts.len() == 2);
-        let unquoted = &*parts[0];
-        let quoted_expected = &*parts[1];
-        let quoted_actual = try_quote(&parts[0]).unwrap();
+            .map(|part| part.trim().trim_start_matches('<').trim_end_matches('>'));
+        let unquoted = parts.next().unwrap();
+        let quoted_expected = parts.next().unwrap();
+        assert_eq!(parts.next(), None);
+        (unquoted.to_owned(), quoted_expected.to_owned())
+    })
+}
+
+#[test]
+#[cfg_attr(not(feature = "std"), allow(unreachable_code, unused_mut))]
+fn test_quote() {
+    let mut ok = true;
+    for (unquoted, quoted_expected) in test_cases() {
+        let quoted_actual = try_quote(&unquoted).unwrap();
         if quoted_expected != quoted_actual {
             #[cfg(not(feature = "std"))]
-            panic!("FAIL: for input <{}>, expected <{}>, got <{}>",
-                     unquoted, quoted_expected, quoted_actual);
+            panic!(
+                "FAIL: for input <{}>, expected <{}>, got <{}>",
+                unquoted, quoted_expected, quoted_actual
+            );
             #[cfg(feature = "std")]
-            println!("FAIL: for input <{}>, expected <{}>, got <{}>",
-                     unquoted, quoted_expected, quoted_actual);
+            println!(
+                "FAIL: for input <{}>, expected <{}>, got <{}>",
+                unquoted, quoted_expected, quoted_actual
+            );
             ok = false;
         }
     }
